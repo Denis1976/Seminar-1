bf65441a94c1d324249bfa5aa28017aaad2acfcf
Denis <info.strakhovka@mail.ru>
2022-11-23 22:29:36 +0300

    Add project Test 2

---
+++ Test_2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Привет! Введи любое целое число!");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int P = -N; P <= N; P++)
+{
+    Console.WriteLine(P);
+}
+Console.WriteLine("Повторим еще раз?");
