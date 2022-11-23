04b49f1ca1d811c420f02407cd5542abd4ab665b
Denis <info.strakhovka@mail.ru>
2022-11-23 19:32:57 +0300

    initial project Test 1

---
+++ Test_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b * b == a)
+{
+    Console.WriteLine("Ура! Первое число является квадратом второго!");
+}
+else
+{
+    Console.WriteLine("Ошибка!");
+}
